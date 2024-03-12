00b666906144d5f23d0e91cf8200593d6d7e1e39
LoneDev <gabquelyn@gmail.com>
2024-03-12 19:09:51 +0000

    Home page trial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function Root() {
+export default function Poot() {
   return (
-    <div>Hello world!</div>
-  )
+    <div>
+      Hello world!
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore fugit enim recusandae delectus odio deserunt beatae repellat
+        ipsum, magnam voluptas iste, quae earum sunt alias cum aliquid
+        reprehenderit veniam.
+      </p>
+    </div>
+  );
 }
